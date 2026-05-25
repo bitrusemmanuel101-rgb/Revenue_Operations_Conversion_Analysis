e4ef0f60d35568aaea093863632cfa65b5ef65a9
bitrusemmanuel101-rgb <bitrusemmanuel101@gmail.com>
2026-05-25 14:55:39 +0100

    first commit

---
+++ Data_Sources/Accounts.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Accounts (
+Account_ID VARCHAR(255),
+Account_Name VARCHAR(255),
+Industry VARCHAR(255),
+Customer_Type VARCHAR(255),
+Region VARCHAR(255),
+Annual_Revenue DECIMAL(15,2)
+);
+
+INSERT INTO Accounts VALUES ('ACC001', 'Brown, Nolan and Cole', 'Retail', 'New', 'West', 2921370);
+INSERT INTO Accounts VALUES ('ACC002', 'Jackson, Nguyen and Anderson', 'Healthcare', 'Existing', 'South', 2935628);
+INSERT INTO Accounts VALUES ('ACC003', 'Hays Inc', 'Retail', 'Existing', 'East', 4604364);
+INSERT INTO Accounts VALUES ('ACC004', 'Krueger, Dean and Shaw', 'Retail', 'New', 'West', 2873956);
+INSERT INTO Accounts VALUES ('ACC005', 'Mcguire-Allen', 'Technology', 'New', 'West', 3451910);
+INSERT INTO Accounts VALUES ('ACC006', 'Ward-Paul', 'Retail', 'Existing', 'South', 1413041);
+INSERT INTO Accounts VALUES ('ACC007', 'Ibarra-Long', 'Technology', 'Existing', 'East', 4468331);
+INSERT INTO Accounts VALUES ('ACC008', 'Parker, Peters and Long', 'Finance', 'Existing', 'West', 1932076);
+INSERT INTO Accounts VALUES ('ACC009', 'Taylor, Brewer and White', 'Retail', 'Existing', 'South', 3579931);
+INSERT INTO Accounts VALUES ('ACC010', 'Johnson and Sons', 'Healthcare', 'Existing', 'South', 3791355);
+INSERT INTO Accounts VALUES ('ACC011', 'Trujillo, Cox and Anderson', 'Finance', 'Existing', 'North', 1682896);
+INSERT INTO Accounts VALUES ('ACC012', 'Clark Inc', 'Finance', 'New', 'West', 4798481);
+INSERT INTO Accounts VALUES ('ACC013', 'Henry-Summers', 'Healthcare', 'New', 'East', 1400205);
+INSERT INTO Accounts VALUES ('ACC014', 'Shaw-Johnson', 'Healthcare', 'Existing', 'North', 2130792);
+INSERT INTO Accounts VALUES ('ACC015', 'Garcia LLC', 'Retail', 'Existing', 'South', 1224564);
+INSERT INTO Accounts VALUES ('ACC016', 'Knapp, Schmidt and Chapman', 'Healthcare', 'Existing', 'East', 3452672);
+INSERT INTO Accounts VALUES ('ACC017', 'Hamilton-Morris', 'Technology', 'Existing', 'North', 1295069);
+INSERT INTO Accounts VALUES ('ACC018', 'Washington Inc', 'Manufacturing', 'New', 'North', 2860622);
+INSERT INTO Accounts VALUES ('ACC019', 'Brown, Hartman and Campbell', 'Technology', 'New', 'South', 1445226);
+INSERT INTO Accounts VALUES ('ACC020', 'Contreras LLC', 'Healthcare', 'Existing', 'North', 1796479);
